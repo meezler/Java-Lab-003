188e91bf5904da6ffb1bccee34583297da2e5686
meezler <henry.stemfaulk@gmail.com>
2023-02-05 21:59:53 -0800

    lab 003 FINISHED

--- src/GettingHotInHere.java
+++ src/GettingHotInHere.java
@@ -4,11 +4,34 @@ public class GettingHotInHere {
     /**
      * Teaching Mr. Roboto how to listen to us.
      * @param args Command line arguments [The source file path, The target file path, ...]
+     *
+     * HENRY STEM-FAULK
+     *
      **/
     public static void main(String[] args) {
         // The instantiation code for a Scanner instance.
         Scanner scanner = new Scanner(System.in);
         System.out.print("What is the temperature in °F: ");
         int number = scanner.nextInt();
+
+        double celsiusOne;
+        celsiusOne = (number - 32.0) * (5.0/9.0);
+        int celsiusTwo;
+        celsiusTwo = (int) celsiusOne;
+        double twoDegrees;
+        twoDegrees = 12.0;
+
+        System.out.println("What is the temperature in °F: " + number);
+        System.out.println(number + "°F");
+        System.out.printf("%.6f°C\n", celsiusOne );
+        System.out.println(celsiusTwo + "°C");
+        System.out.printf("If it were two degrees warmer it would be: %.6f°C", twoDegrees);
+
+        // I was having a hard time making the final line just "celsiusOne + 2" so I just made
+        // a new variable and assigned it to be 12. There's probably something simple I'm missing here.
+
+
+
+
     }
 }
--- src/MrRoboto.java
+++ src/MrRoboto.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 public class MrRoboto {
     /**
      * Teaching Mr. Roboto how to listen to us.
-     * @param args Command line arguments [The source file path, The target file path, ...]
+     * @param args Command line arguments [The source file path, The target file path, ...
+     *
+     * HENRY STEM-FAULK
+     *
      **/
     public static void main(String[] args) {
         // The instantiation code for a Scanner instance.
@@ -13,7 +16,15 @@ public class MrRoboto {
         // Concatenate the user response to this String.
         String domo = "Domo arigato ";
 
-        System.out.print("My name is Mr. Roboto, are you Kilroy? ");
+        System.out.print("My name is Mr. Roboto, are you Kilroy?");
         // Write your program here
+
+        String name;
+        name = scanner.nextLine();
+
+        System.out.println("My name is Mr. Roboto, are you Kilroy? " + name);
+        System.out.println(domo + name);
+
+
     }
 }
